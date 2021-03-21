6b003a5dd3eb5cab0399177248cb42d9ea1fb551
BIT-Garage <mailbitgarage@gmail.com>
2021-03-21 13:15:40 +0545

    added db and contact

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,14 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('about',views.about, name='about'),
+    path('blog_home',views.blog_home, name='about'),
+    path('blog_single',views.blog_single, name='blog_single'),
+    path('contact',views.contact, name='contact'),
+    path('elements',views.elements, name='elements'),
+    path('portfolio',views.portfolio, name='portfolio'),
+    path('price',views.price, name='price'),
+    path('services',views.services, name='services'),
+
+
 ]
